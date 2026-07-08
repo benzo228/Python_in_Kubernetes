5a709b2d8cb58b7a9035846ace529c93c91f0da7
BigBrother628 <ivvlad2005@gmail.com>
2026-07-08 21:34:57 +0000

    a lot of things

--- app/main.py
+++ app/main.py
@@ -123,4 +123,4 @@ async def frontend(request: Request):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", "8000"))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
